284126f3d1fb4ffd0f32dcefe893b8df7b3d7886
Choi Jae Young <chlwodud0327@naver.com>
2024-02-20 11:51:41 +0900

    Feat: 도서관리 프로그램 V4
    도서 등록 메서드 수정
    주석 정리

--- src/main/java/homework/exam02/BookMain.java
+++ src/main/java/homework/exam02/BookMain.java
@@ -28,11 +28,7 @@ public class BookMain {
                     int bookType = sc.nextInt();
                     sc.nextLine(); // 버퍼 비우기
 
-                    if (bookType == 1) { // 전자책이라면
-                        saveEBook(sc, bookList);
-                    } else if (bookType == 2) { // 종이책이라면
-                        savePaperBook(sc, bookList);
-                    }
+                    saveBook(sc,bookType,bookList);
                     System.out.println();
                 }
                 case 2 -> { // 도서 검색(책 제목 or 책 저자)
@@ -73,7 +69,7 @@ public class BookMain {
                     BookClass book = buyTheBook(bookType, title, bookList);
 
                     if (book == null) System.out.println("구매 하실 책이 존재하지 않습니다 !!");
-                    else bookList.remove(book); // 구매했으니 도서목록에서 삭제
+                    else bookList.remove(book);
 
                     System.out.println();
                 }
@@ -87,53 +83,72 @@ public class BookMain {
         sc.close();
     }
 
-    // 종이책 저장을 위한 메서드
-    private static void savePaperBook(Scanner sc, List<BookClass> bookList) {
+    /**
+     * <h1>책 저장 메서드</h1>
+     * 입력을 위한 Scanner 와 전자책인지 종이책인지 구분할 type, List 타입의 bookList 를 파라미터로 받는다.
+     * <li>case 1 번에서 사용</li>
+     */
+    private static void saveBook(Scanner sc, int type, List<BookClass> bookList) {
         BookClass book = inputBook(sc);
-        System.out.print("쪽수 입력하세요 : ");
-        int size = sc.nextInt();
-        sc.nextLine();
-        bookList.add(new PaperBookClass(book.getTitle(), book.getAuthor(), book.getPrice(), book.getPublisher(), size));
-        System.out.println("책 저장이 완료 되었습니다 !!");
+        if (type == 1) { // 전자책
+            System.out.print("쪽수 입력하세요 : ");
+            int size = sc.nextInt();
+            sc.nextLine();
+            bookList.add(new PaperBookClass(book.getTitle(), book.getAuthor(), book.getPrice(), book.getPublisher(), size));
+            System.out.println("책 저장이 완료 되었습니다 !!");
+        } else if (type == 2) { // 종이책
+            System.out.print("호환기기를 입력하세요 : ");
+            String superDevices = sc.nextLine();
+            EBookClass eBookClass = new EBookClass(book.getTitle(), book.getAuthor(), book.getPrice(), book.getPublisher(), superDevices);
+            bookList.add(eBookClass);
+            System.out.println("책 저장이 완료 되었습니다 !!");
+        }
     }
 
-    // 전자책 저장을 위한 메서드
-    private static void saveEBook(Scanner sc, List<BookClass> bookList) {
-        BookClass book = inputBook(sc);
-        System.out.print("호환기기를 입력하세요 : ");
-        String superDevices = sc.nextLine();
-        EBookClass eBookClass = new EBookClass(book.getTitle(), book.getAuthor(), book.getPrice(), book.getPublisher(), superDevices);
-        bookList.add(eBookClass);
-        System.out.println("책 저장이 완료 되었습니다 !!");
-    }
-
-    // 책 제목 or 책 저자가 찾는 키워드와 같으면 객체 반환
+    /**
+     * <h1>책 단권 검색 메서드</h1>
+     * 검색할 책 제목인 keyword , List 타입의 bookList를 파라미터로 받는다.
+     * <li>case 2 번에서 사용</li>
+     */
     private static BookClass searchBook(String keyword, List<BookClass> bookList) {
         for (BookClass book : bookList) {
             if (book.getTitle().equals(keyword) || book.getAuthor().equals(keyword)) {
                 return book;
             }
         }
-        return null; // 찾지 못했을 경우 null 
+        return null;
     }
 
-    // 책 제목을 검색하여 책을 반환
+    /**
+     * <h1>책 제목을 이용하여 검색하는 메서드</h1>
+     * 책 제목인 title 와 List타입의 bookList를 파라미터로 받아 title을 가지는 객체를 찾아서 반환 못찾으면 null 반환
+     * <li>case 3 번에서 사용</li>
+     */
     private static BookClass findBook(String title, List<BookClass> bookList) {
         for (BookClass book : bookList) {
-            if (book.getTitle().equals(title)) { // 책 제목과 찾는책 제목이 같다면 객체 반환
+            if (book.getTitle().equals(title)) {
                 return book;
             }
         }
-        return null; // 찾지 못했을 경우 null 
+        return null;
     }
 
-    // 종이책인지 전자책인지 구분하여 각 책의 정보를 출력
+    /**
+     * <h1>자식타입 추론을 위한 메서드</h1>
+     * 파라미터로 BookClass(부모타입) 참조변수를 받아 자식타입 비교후 자식타입의 정보를 출력
+     * <li>case 2,4 번에서 사용</li>
+     */
     private static void bookInfo(BookClass book) {
         if (book instanceof EBookClass eBook) eBook.deviceInfo();
         else if (book instanceof PaperBookClass paperBook) paperBook.sizeInfo();
     }
 
-    // 책 구입을 위한 메서드
+    /**
+     * <h1>책 구입을 위한 메서드</h1>
+     * 전자책인지 종이책인지 구분하는 bookType 와 책제목 title, List 타입의 bookList를 파라미터로 받아 각 종류마다 로직 실행
+     * 반환은 해당 타입의 객체를 반환 구입할 책이 없다면 null 반환
+     * <li>case 5 번에서 사용</li>
+     */
     private static BookClass buyTheBook(String bookType, String title, List<BookClass> bookList) {
         if (bookType.equals("전자책")) {
             for (BookClass b : bookList) {
@@ -156,7 +171,8 @@ public class BookMain {
     }
 
     /**
-     * 반복하는 코드 메서드 추출
+     * <h1>책 등록 시 공통 필드 입력받는 메서드</h1>
+     * Scanner 를 파라미터로 받아 부모객체 필드를 입력받는 로직을 실행하고 입력값으로 부모타입의 객체를 생성 후 반환
      */
     private static BookClass inputBook(Scanner sc) {
         System.out.print("제목을 입력하세요 : ");
